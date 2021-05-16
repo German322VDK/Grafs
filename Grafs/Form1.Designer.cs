50372f1d1c05a3dbbfa41e6b46003537c422ed78
гера шлычков <germean322@gmail.com>
2021-05-16 15:13:13 +1000

    Добавьте файлы проекта.

---
+++ Grafs/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Grafs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(15, 34);
+            this.chart1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series3.LabelForeColor = System.Drawing.SystemColors.Highlight;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.LabelForeColor = System.Drawing.Color.Orange;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(601, 354);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1032, 338);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(646, 36);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 21);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "ср кв";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(768, 36);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(94, 21);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ср произв";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(910, 36);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(98, 21);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "мет перем";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(1027, 36);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(108, 21);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "лин кон мет";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(910, 115);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1024, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Количество чисел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(769, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Разрядность";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(646, 112);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(646, 172);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(108, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(769, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Случайное число 1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(646, 293);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(372, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1024, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Окно ошибок";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(769, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Случайное число 2";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(646, 220);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(108, 22);
+            this.textBox5.TabIndex = 15;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(910, 172);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(108, 22);
+            this.textBox6.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1024, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(118, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "верхняя граница";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(910, 222);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(108, 22);
+            this.textBox7.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1024, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 17);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "a";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Лаб 5";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Grafs/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Grafs
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// count - Количество чисел
+        /// a - массив значений (разрядное число)
+        /// r - массив значений (дробное число)
+        /// ran - массив с# rand значений 
+        /// bitness - разрядность
+        /// N - основание
+        /// </summary>
+        private long count, bitness;
+        private long[] a;
+        private double[] r, ran;
+        private double pw;
+        private long M;
+
+        private double A;
+        private double b;
+
+        private const int N = 10;
+        private const string ser2 = "C# Rand";
+
+        //функция выбора метода
+        public int RadButton()
+        {
+            if (radioButton1.Checked) return 1;
+            if (radioButton2.Checked) return 2;
+            if (radioButton3.Checked) return 3;
+            if (radioButton4.Checked) return 4;
+
+            return 0;
+        }
+
+        //метод срединных квадратов
+        public void MiddleSqure()
+        {
+            //подписываем график
+            chart1.Series["Series1"].LegendText = "mid squ";
+            //объект рандома
+            Random rand = new Random();
+            //получение первого значение массива r и массива ran
+            r[0] = a[0] / pw;
+            ran[0] = rand.NextDouble();
+            //цикл вычесления r и ran
+            for (int i = 1; i < count; i++)
+            {
+                //возводим в квадрат и отбрасываем последние числа
+                a[i] = (long)((a[i-1] * a[i-1]) / Math.Pow(N, bitness/2));
+                //отбрасываем первые числа
+                a[i] = (long)(a[i] % pw);
+                //вычесляем r
+                r[i] = a[i] / pw;
+
+                if (r[i] < 0) 
+                    throw new ArgumentException("Получилось слишком большое число");
+                //вычесляем ran
+                ran[i] = rand.NextDouble();
+            }
+
+            //отрисовка графиков
+            for(int i = 0; i < count; i++)
+            {
+                r[i] = Math.Round(r[i], 3);
+                ran[i] = Math.Round(ran[i], 3);
+
+                chart1.Series["Series1"].Points.AddXY(i, r[i]);
+                chart1.Series["Series2"].Points.AddXY(i, ran[i]);
+            }
+        }
+
+        //метод срединных произведений
+        public void MiddleMultyPly()
+        {
+            //подписываем график
+            chart1.Series["Series1"].LegendText = "mid mult";
+            //объект рандома
+            Random rand = new Random();
+            //вычесление первого и второго r
+            r[0] = a[0] / pw;
+            r[1] = a[1] / pw;
+            //вычесление первого и второго ran
+            ran[0] = rand.NextDouble();
+            ran[1] = rand.NextDouble();
+            //цикл вычесления r и ran
+            for (int i = 2; i < count; i++)
+            {
+                //перемножаем и отбрасываем последние числа
+                a[i] = (long)((a[i - 1] * a[i - 2]) / Math.Pow(N, bitness / 2));
+                //отбрасываем первые числа
+                a[i] = (long)(a[i] % pw);
+                //вычесляем r
+                r[i] = a[i] / pw;
+
+                if (r[i] < 0)
+                    throw new ArgumentException("Получилось слишком большое число");
+                //вычесляем ran
+                ran[i] = rand.NextDouble();
+            }
+            //отрисовка графиков
+            for (int i = 0; i < count; i++)
+            {
+                r[i] = Math.Round(r[i], 3);
+                ran[i] = Math.Round(ran[i], 3);
+
+                chart1.Series["Series1"].Points.AddXY(i, r[i]);
+                chart1.Series["Series2"].Points.AddXY(i, ran[i]);
+            }
+        }
+
+        //метод перемешивания
+        public void MethMixing()
+        {
+            //подписываем график
+            chart1.Series["Series1"].LegendText = "meth mix";
+            //объект рандома
+            Random rand = new Random();
+            //переменные сдвига влево и вправо
+            long a1, a2;
+            //переменные для сдвига влево и вправо
+            long mx1 = (int)Math.Pow(N, (3 * bitness) / 4);
+            long mx2 = (int)Math.Pow(N, bitness / 4);
+            //получение первого значение массива r и массива ran
+            r[0] = a[0] / pw;
+            ran[0] = rand.NextDouble();
+            //цикл вычесления r и ran
+            for (int i = 1; i < count; i++)
+            {
+                a1 = (a[i-1] % mx1) * mx2 + (a[i - 1] / mx1); //сдвиг влево
+                a2 = (a[i - 1] % mx2) * mx1 + (a[i - 1] / mx2); //сдвиг вправо
+                //отбрасываем первые числа
+                a[i] = (long)((a1 + a2) % pw);
+                //отбрасываем последние числа, вычесляем r
+                r[i] = a[i] / pw;
+
+                if (r[i] < 0)
+                    throw new ArgumentException("Получилось слишком большое число");
+                //вычесляем ran
+                ran[i] = rand.NextDouble();
+            }
+            //отрисовка графиков
+            for (int i = 0; i < count; i++)
+            {
+                r[i] = Math.Round(r[i], 3);
+                ran[i] = Math.Round(ran[i], 3);
+
+                chart1.Series["Series1"].Points.AddXY(i, r[i]);
+                chart1.Series["Series2"].Points.AddXY(i, ran[i]);
+            }
+        }
+
+        //Линейный конгруэнтный метод
+        public void LineKon()
+        {
+            //подписываем график
+            chart1.Series["Series1"].LegendText = "line core";
+            chart1.Series["Series2"].LegendText = "";
+
+            b = 0.21131 * M;
+
+            for (int i = 1; i < count; i++)
+            {
+                r[i] = (int)( ( A * r[i-1] + b) % M);
+            }
+
+                //отрисовка графика
+            for (int i = 0; i < count; i++)
+            {
+                chart1.Series["Series1"].Points.AddXY(i, r[i]);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Отчиска окна графиков
+                chart1.Series["Series1"].Points.Clear();
+                chart1.Series["Series2"].Points.Clear();
+                chart1.Series["Series2"].LegendText = ser2;
+
+                int sw = RadButton(); //Выбранный метод
+
+                count = Convert.ToInt32(textBox1.Text);
+
+                // проверки на корректность
+                if (count < 2)
+                    throw new Exception("Количество чисел");
+
+                //создание массивов
+                a = new long[count + 1];
+                r = new double[count + 1];
+                ran = new double[count + 1];
+
+                if (sw != 4)
+                {
+                    bitness = Convert.ToInt32(textBox2.Text);
+
+                    if (bitness % 2 != 0)
+                        throw new Exception("Разрядность");
+
+                    //начальное значение
+                    a[0] = Convert.ToInt32(textBox3.Text);
+
+                    //максимальное значение разрядного числа
+                    pw = Math.Pow(N, bitness);
+
+                    //проверка соответсвия разраядности
+                    if (a[0] >= pw || a[0] < pw / 10)
+                        throw new Exception("число 1 не соответствует разрядности");
+
+                    if (sw == 2)
+                    {
+                        //второе значение
+                        a[1] = Convert.ToInt32(textBox5.Text);
+
+                        if (a[1] >= pw || a[1] < pw / 10)
+                            throw new Exception("число 2 не соответствует разрядности");
+                    }
+                }
+
+                else
+                {
+                    M = Convert.ToInt64(textBox6.Text);
+
+                    if (M < 1000)
+                        throw new Exception("Должно быть больше 1000");
+
+                    A = Convert.ToDouble(textBox7.Text);
+
+                    if ((A <= M / 100 || A >= (M - Math.Sqrt(M))) || (A % 8) != 5 )
+                        throw new Exception("A некоректно");
+
+                    r[0] = Convert.ToDouble(textBox3.Text);
+
+                    if(r[0] < 0 || r[0] > M)
+                        throw new Exception("начальное число некоректно");
+                }
+
+                //Выбор запускаемого метода
+                switch (sw)
+                {
+                    case 1: MiddleSqure(); break;
+                    case 2: MiddleMultyPly();  break;
+                    case 3: MethMixing();  break;
+                    case 4: LineKon();  break;
+                    default: throw new Exception("Не выбран метод");
+                }
+
+                textBox4.Text = "Введены коректные данные";
+            }
+            catch (Exception er)
+            {
+                //обработка ошибок
+                textBox4.Text = er.Message;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
